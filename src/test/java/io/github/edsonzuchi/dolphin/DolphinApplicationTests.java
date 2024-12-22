f1e5dbbf3b43e2335395dceace9902d0f486680e
Edson Zucchi <edsonluiz.zuchi@gmail.com>
2024-12-22 11:15:29 -0300

    first commit

---
+++ src/test/java/io/github/edsonzuchi/dolphin/DolphinApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.edsonzuchi.dolphin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolphinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
